75ae4690a6a2674ff6f4892be3bea7c44a81b807
DamjanKolevski <damjan.kolevski@gmail.com>
2021-03-05 15:09:20 +0100

    edit

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
     $('.slider').slick({
         dots: true
     });
+
+    $('.slick-slide>img')
+        .wrap('<span style="display:inline-block"></span>')
+        .css('display', 'block')
+        .parent()
+        .zoom();
+
 });
